6a8aad39da8f02e20a94f6b680191f7af1ca1d3c
kstallworth2012 <kennethstallworth@hotmail.com>
2023-02-10 12:48:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+	console.log("Hello dude welcome to my project template")
+	let firstHeader = document.querySelector("h1")
+	firstHeader.innerText = "WELCOME TO THE START OF A GREAT PROJECT!"
+})
